0a18a793086882a9807cc3c1824b6ae74a5d43ab
Ashish Binu <ashishb@carestack.com>
2024-02-01 10:18:22 +0530

    feat: pagination works

--- components/ProductList/ProductList.js
+++ components/ProductList/ProductList.js
@@ -2,24 +2,55 @@ import productListComponentHTML from "./ProductList.html?raw";
 
 export default class ProductListComponent extends HTMLElement {
   static tagName = "product-list-component";
+  pageSize = 10; // Default page size
+  products = [];
+
   constructor() {
     super();
     this.innerHTML = productListComponentHTML;
+
+    this.paginationWidget = document.querySelector("pagination-widget");
+    this.paginationWidget.setAttribute("page-size", this.pageSize.toString());
+    this.paginationWidget.addEventListener(
+      "page-selected",
+      this.handlePageSelected.bind(this),
+    );
   }
 
   connectedCallback() {
+    this.fetchProducts();
+  }
+
+  fetchProducts() {
     fetch("../../data.json").then((response) => response.json()).then(
       (products) => {
-        console.log(products);
-        const parent = document.querySelector(".main__product-list");
-        products.forEach((product) => {
-          const childHTML = `<product-card-component image-url="${
-            product.url[0]
-          }" product-name="${product.name}" product-company="${product.company}" product-price="${product.price}" product-id="${product.id}"></product-card-component>`;
-          parent.insertAdjacentHTML("beforeend", childHTML);
-        });
+        this.products = products;
+        this.paginationWidget.setAttribute(
+          "total-items",
+          products.length.toString(),
+        );
+        this.displayPage(1); // Display the first page initially
       },
     ).catch((err) => console.error(err));
+  }
+  displayPage(pageNumber) {
+    const startIndex = (pageNumber - 1) * this.pageSize;
+    const endIndex = Math.min(startIndex + this.pageSize, this.products.length);
+    const productsToShow = this.products.slice(startIndex, endIndex);
+
+    const parent = this.querySelector(".main__product-list");
+    parent.innerHTML = ""; // Clear the current products
+    productsToShow.forEach((product) => {
+      const childHTML = `<product-card-component image-url="${
+        product.url[0]
+      }" product-name="${product.name}" product-company="${product.company}" product-price="${product.price}" product-id="${product.id}"></product-card-component>`;
+      parent.insertAdjacentHTML("beforeend", childHTML);
+    });
+  }
+
+  handlePageSelected(event) {
+    const selectedPage = event.detail.pageNumber;
+    this.displayPage(selectedPage);
   }
 }
 
